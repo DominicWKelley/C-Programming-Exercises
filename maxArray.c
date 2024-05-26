913846fc1f4985351c6f20dfa02fba92cd5fc541
DominicWKelley <dkwelding@hotmail.com>
2024-05-25 19:36:47 -0700

    New Linux Setup

---
+++ maxArray.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int myArray[] = {8,6,5,2,4,9,0,5,7};
+    int max = myArray[0];
+
+    for (int i = 0; i < 9; i++)
+    {
+        if (myArray[i] > max) max = myArray[i];
+    }
+
+    printf("max: %d\n", max);
+
+    return 0;
+}
