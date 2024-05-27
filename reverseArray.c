da244777c11699eedf6fed2a175f497fa0a50328
DominicWKelley <dkwelding@hotmail.com>
2024-05-26 19:56:43 -0700

    5-26-24

---
+++ reverseArray.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int myArray[] = {1,2,3,4,5,6,7,8,9,10};
+    int length = 10;
+    int temp = 0;
+
+    for (int i = 0; i < (length / 2); i++)
+    {
+        temp = myArray[i];
+        myArray[i] = myArray[length - i - 1];
+        myArray[length - i - 1] = temp;
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        printf("myArray[%d] = %d\n", i, myArray[i]);
+    }
+
+    return 0;
+}
