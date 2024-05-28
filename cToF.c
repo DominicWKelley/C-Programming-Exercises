1d5fc725cf7b5e814c80686eb58329dcba8cb000
DominicWKelley <dkwelding@hotmail.com>
2024-05-27 21:02:30 -0700

    5-27-24

---
+++ cToF.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double C = 0;
+    double F = 0;
+
+    printf("Enter C temp: \n");
+    scanf("%lf", &C);
+
+    F = (C * 1.8) + 32;
+
+    printf("C: %.2lf -> F %.2lf\n", C, F);
+
+    return 0;
+}
