3faee76f636cd25afeefb895f147c803091b6012
DominicWKelley <dkwelding@hotmail.com>
2024-06-12 18:28:54 -0700

    6/12/24

---
+++ rotateArrayRight.c
@@ -0,0 +1,52 @@
+// Portfolio Courses C Programming Examples #31
+
+#include <stdio.h>
+
+// before
+//
+// index:  0  1  2  3  4  5
+// array:  3  9  8  1  7  6
+//
+// index:  0  1  2  3  4  5
+// array:  6  3  9  8  1  7
+//
+
+void rotate_right_once(int array[], int length);
+void rotate_right(int array[], int length, int n);
+
+int main(void)
+{
+    int a1[] = {1,2,3,4,5,6};
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("a1 before: %d\n", a1[i]);
+    }
+
+    rotate_right(a1, 6, 2);
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("a1 after: %d\n", a1[i]);
+    }
+
+    return 0;
+}
+
+void rotate_right_once(int array[], int length)
+{
+    int temp = array[length - 1];
+    for (int i = (length - 1); i > 0; i--)
+    {
+        array[i] = array[i - 1];
+    }
+    array[0] = temp;
+}
+
+void rotate_right(int array[], int length, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        rotate_right_once(array, length);
+    }
+}
--- validatePostCode.c
+++ validatePostCode.c
@@ -1,3 +1,5 @@
+// Portfolio Courses C Programming Examples #25
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
