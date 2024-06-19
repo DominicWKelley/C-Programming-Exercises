c100ff8990277bb4e37ccc2b401133bc33015639
DominicWKelley <dkwelding@hotmail.com>
2024-06-18 19:49:46 -0700

    6/18/24

---
+++ recursiveSum.c
@@ -0,0 +1,19 @@
+// Portfolio Courses C Programming Examples #37
+// Sum The First N Natural Numbers Using Recursion.
+
+#include <stdio.h>
+
+int sum(int n);
+
+int main(void)
+{
+    printf("%d\n", sum(5));
+
+    return 0;
+}
+
+int sum(int n)
+{
+    if (n > 0) return n + sum(n-1);
+    else return 0;
+}
