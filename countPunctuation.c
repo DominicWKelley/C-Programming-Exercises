63294ea6a440e9a848d84fda6def2a200b128cdd
DominicWKelley <35047973+DominicWKelley@users.noreply.github.com>
2024-06-30 12:08:01 -0700

    6/30/24

---
+++ countPunctuation.c
@@ -0,0 +1,26 @@
+// Portfolio Courses C Programming Examples #47
+// Count the punctuation marks in a string.
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int count__punct(char *s);
+
+int main(void)
+{
+    char s[] = "apople, pear, orange; something...";
+    int total = count__punct(s);
+    printf("total: %d\n", total);
+
+    return 0;
+}
+
+int count__punct(char *s)
+{
+    int len = strlen(s);
+    int count = 0;
+    for (int i = 0; i < len; i++)
+        if (ispunct(s[i])) count++;
+    return count;
+}
