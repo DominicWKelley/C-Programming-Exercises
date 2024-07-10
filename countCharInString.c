795352482e7bc6c5c6f0cd24af7db9555b9581b5
DominicWKelley <35047973+DominicWKelley@users.noreply.github.com>
2024-07-09 19:15:33 -0700

    Tue, 9Jul24

---
+++ countCharInString.c
@@ -0,0 +1,41 @@
+// Portfolio Courses C Programming Examples #52
+// Count occurrences of each character in a string.  
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+void print_occurrences(char *s);
+
+int main(void)
+{
+  char s[] = "abcdefghijklmnopabcdefabcab";
+  print_occurrences(s);
+
+  return 0;
+}
+
+void print_occurrences(char *s)
+{
+  int length = strlen(s);
+  char unique[length];
+  int counted = 0;
+
+  for (int i = 0; i < length; i++)
+  {
+    bool already_counted = false;
+    for (int j = 0; j < counted; j++)
+      if (s[i] == unique[j])
+        already_counted = true;
+    if (already_counted) continue;
+
+    int count = 0;
+    for (int j = 0; j <length; j++)
+      if (s[i] == s[j]) count++;
+
+    printf("%c - %d\n", s[i], count);
+
+    unique[counted] = s[i];
+    counted++;
+  }
+}
