2cee6f9a5a06072d08601000b097c12a3dff7b31
DominicWKelley <35047973+DominicWKelley@users.noreply.github.com>
2024-07-19 14:14:29 -0700

    Fri, 19Jul24

---
+++ remCharAtPosInString.c
@@ -0,0 +1,39 @@
+// Portfolio Courses C Programming Examples #60
+// Remove a character from a string at a specified position.
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+bool remove_char(char *s, int pos);
+
+int main(void) {
+  char s[] = "12345-12345";
+
+  bool result1 = remove_char(s, 20);
+  if (result1)
+    printf("Char removed from string!\n");
+  else
+    printf("Char not removed from string!\n");
+
+  bool result2 = remove_char(s, 5);
+  if (result2)
+    printf("Char removed from string!\n");
+  else
+    printf("Char not removed from string!\n");
+
+  printf("%s\n", s);
+
+  return 0;
+}
+
+bool remove_char(char *s, int pos) {
+  int length = strlen(s);
+
+  if (pos >= length)
+    return false;
+  for (int i = pos; i < length; i++) {
+    s[i] = s[i + 1];
+  }
+  return true;
+}
