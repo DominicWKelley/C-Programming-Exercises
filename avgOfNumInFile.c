83ee5dad777a026a64ab7da8e5906648173eb74b
DominicWKelley <35047973+DominicWKelley@users.noreply.github.com>
2024-07-31 15:09:42 -0700

    Wed, 31Jul24

---
+++ avgOfNumInFile.c
@@ -0,0 +1,32 @@
+// Portfolio Courses C Programming Examples #70
+// Find the largest number in a file.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BSIZE 1024
+
+int main(void) {
+  FILE *fh;
+  char buffer[BSIZE];
+  double average, sum = 0;
+  int total = 0;
+
+  fh = fopen("avg.txt", "r");
+
+  if (fh == NULL) {
+    printf("Error opening file.\n");
+    return 1;
+  }
+
+  while (fgets(buffer, BSIZE, fh) != NULL) {
+    sum += atof(buffer);
+    total++;
+  }
+
+  average = sum / total;
+
+  printf("average: %.2f\n", average);
+
+  return 0;
+}
