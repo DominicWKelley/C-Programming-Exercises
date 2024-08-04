f4cc6743dded0109243a02d6d4ed42df866649b6
DominicWKelley <35047973+DominicWKelley@users.noreply.github.com>
2024-08-03 20:37:24 -0700

    Sat, 3Aug24

---
+++ appendTextToFile.c
@@ -0,0 +1,33 @@
+// Portfolio Courses C Programming Examples #74
+// Append text to a file.
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LINE 1024
+
+int main(void) {
+  FILE *file;
+  char buffer[MAX_LINE];
+
+  file = fopen("file.txt", "a");
+
+  if (file == NULL) {
+    printf("Error opening file.\n");
+    return 1;
+  }
+
+  printf("Enter 'quit' to exit.\n");
+  do {
+    fgets(buffer, MAX_LINE, stdin);
+    if (strcmp(buffer, "quit\n") == 0) {
+      break;
+    }
+    fputs(buffer, file);
+  } while (true);
+
+  fclose(file);
+
+  return 0;
+}
